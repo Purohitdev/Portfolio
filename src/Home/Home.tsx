988f72091362743c7013fc6efba480df64e0b7df
Purohitdev <devp61285@gmail.com>
2025-03-07 17:39:04 +0530

    error solve

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
-import Nav from "../Nav";
 import Hero from "./Hero";
 import About from "./About";
 import Projects from "./Projects";
